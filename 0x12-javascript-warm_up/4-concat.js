ee04f976d5d2ee7e3734821820e91634595e5d18
Sammie05 <snweke98@gmail.com>
2024-02-12 17:02:26 +0100

    updated

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
+const arg0 = process.argv[2];
+const arg1 = process.argv[3];
 
-//   A script that prints two arguments
-//   passed to it, in the following format: "is "
-
-const arg1 = process.argv[2] || 'undefined';
-const arg2 = process.argv[3] || 'undefined';
-
-if (process.argv[2] && process.argv[3]) {
-   console.log('${process.argv[2]} is ${process.argv[3]}');
-} else {
-  console.log('${arg1} is ${arg2}');
-}
+console.log(arg0 + ' is ' + arg1);
