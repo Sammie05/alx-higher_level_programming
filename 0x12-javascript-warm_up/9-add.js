86ddb08cccc71a8a06360477034dfeb4e558786f
Sammie05 <snweke98@gmail.com>
2024-02-12 17:21:04 +0100

    updated

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,21 +1,15 @@
 #!/usr/bin/node
 
-/*
-    A script that prints the addition of 2 integers.
-
-    prototype: function add(a, b)
-    The first argument (num1) is the first integer
-    The second argument (num2) is the second integer
-*/
-
-const add = (a, b) => a + b;
-
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-   const result = add(num1, num2);
-   console.log('${result}');
-} else {
-  console.log('NaN');
+/**
+ *  * add - Computes the sum of 2 numbers.
+ *   * @param {Number} a - The first number.
+ *    * @param {Number} b - The second number.
+ *     *
+ *      * @returns The sum of the 2 numbers.
+ *       */
+function add (a, b) {
+	  return a + b;
 }
+console.log(
+	  add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
+);
