9ab5217e52a9a858e66ceac99e1f4963b7dad4ed
Sammie05 <snweke98@gmail.com>
2024-02-12 17:31:26 +0100

    updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-
-/*   A script that searches the second biggest integer in the list of arguments.
-     You can assume all arguments can be converted to integer
-     if no argument passed, print 0
-     if the number of arguments is 1, print 0
-
-*/
-const FindSecondlargest = (...nums) => {
-  if (nums.length < 2) {
-    return 0;
-}
+const args = process.argv
+  .slice(2)
+  .map(arg => Number.parseInt(arg))
+  .sort((a, b) => b - a);
+const val = args.length < 2 ? 0 : args[1];
+console.log(val);
